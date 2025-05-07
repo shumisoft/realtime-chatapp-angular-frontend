f40492e82ca51e3dbe65cf371a6aa6f17e932b69
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2025-05-07 21:56:48 +0530

    chore: integrated NgRx store, effects, and HTTP client into appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideHttpClient } from '@angular/common/http';
+import { authReducer } from './core/store/auth/auth.reducer';
+import { AuthEffects } from './core/store/auth/auth.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore({
+      auth: authReducer,
+    }),
+    provideEffects([AuthEffects]),
+  ],
 };
